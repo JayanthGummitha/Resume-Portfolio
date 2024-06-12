9fad9f34a1dd1001a582f6c8a1411268621d1fc4
JayanthGummitha <jayanthgummitha@gmail.com>
2024-06-11 20:32:10 -0500

    initial commit

---
+++ src/components/Projects/index.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import { useState } from 'react'
+import { Container, Wrapper, Title, Desc, CardContainer, ToggleButtonGroup, ToggleButton, Divider } from './ProjectsStyle'
+import ProjectCard from '../Cards/ProjectCards'
+import { projects } from '../../data/constants'
+
+
+const Projects = ({openModal,setOpenModal}) => {
+  const [toggle, setToggle] = useState('all');
+  return (
+    <Container id="projects">
+      <Wrapper>
+        <Title>Projects</Title>
+        <Desc>
+          I have worked on a wide range of projects. From web application to web apps. Here are some of my projects.
+        </Desc>
+        <ToggleButtonGroup >
+          {toggle === 'all' ?
+            <ToggleButton active value="all" onClick={() => setToggle('all')}>All</ToggleButton>
+            :
+            <ToggleButton value="all" onClick={() => setToggle('all')}>All</ToggleButton>
+          }
+          <Divider />
+          {toggle === 'Frontend app' ?
+            <ToggleButton active value="Frontend app" onClick={() => setToggle('Frontend app')}>FRONT-END APP'S</ToggleButton>
+            :
+            <ToggleButton value="Frontend app" onClick={() => setToggle('Frontend app')}>FRONT-END APP'S</ToggleButton>
+          }
+          <Divider />
+          {toggle === 'Backend app' ?
+            <ToggleButton active value="Backend app" onClick={() => setToggle('Backend app')}>BACK-END APP'S</ToggleButton>
+            :
+            <ToggleButton value="Backend app" onClick={() => setToggle('Backend app')}>BACKEND APP'S</ToggleButton>
+          }
+          <Divider />
+          { toggle === 'Java Full Stack app' ?
+            <ToggleButton active value="Java Full Stack app" onClick={() => setToggle('Java Full Stack app')}>JAVA FULL STACK APP</ToggleButton>
+            :
+            <ToggleButton value="Java Full Stack app" onClick={() => setToggle('Java Full Stack app')}>JAVA FULL STACK APP</ToggleButton>
+          } 
+        </ToggleButtonGroup>
+        <CardContainer>
+          {toggle === 'all' && projects
+            .map((project) => (
+              <ProjectCard project={project} openModal={openModal} setOpenModal={setOpenModal}/>
+            ))}
+          {projects
+            .filter((item) => item.category == toggle)
+            .map((project) => (
+              <ProjectCard project={project} openModal={openModal} setOpenModal={setOpenModal}/>
+            ))}
+        </CardContainer>
+      </Wrapper>
+    </Container>
+  )
+}
+
+export default Projects
